78d3f1559d2ce2bbcf77224020351e12690edf5c
Christo Kurian Tomy <79702220+chriztotomy@users.noreply.github.com>
2022-12-14 09:56:01 +0530

    Add files via upload

---
+++ TrafficDemo.java
@@ -0,0 +1,79 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+//import javafx.application.Application;
+
+class TrafficLight extends JPanel implements ActionListener
+{
+	static JRadioButton r1,r2,r3;
+	static Color red1,green1,orange1;
+	public void TrafficLight()
+	{
+		setBounds(0,0,600,480);
+		r1=new JRadioButton("RED");
+		r2=new JRadioButton("GREEN");
+		r3=new JRadioButton("ORANGE");
+		ButtonGroup group=new ButtonGroup();
+		r1.setSelected(true);
+		group.add(r1);
+		group.add(r2);
+		group.add(r3);
+		add(r1);
+		add(r2);
+		add(r3);
+		red1=Color.red;
+		green1=getBackground();
+		orange1=getBackground();
+		r1.addActionListener(this);
+		r2.addActionListener(this);
+		r3.addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		if(r1.isSelected()==true)
+		{
+			red1=Color.red;
+			green1=getBackground();
+			orange1=getBackground();
+		}
+		else if(r2.isSelected()==true)
+		{
+			red1=getBackground();
+			green1=Color.green;
+			orange1=getBackground();
+		}
+		else if(r3.isSelected()==true)
+		{
+			red1=getBackground();
+			green1=getBackground();
+			orange1=Color.orange;
+		}
+		repaint();
+	}
+	public void paintComponent(Graphics g) //to draw graphics on the screen
+	{
+		g.drawOval(50,50,50,50);
+		g.drawOval(50,110,50,50);
+		g.drawOval(50,170,50,50);
+		g.setColor(red1);
+		g.fillOval(50,50,50,50);
+		g.setColor(green1);
+		g.fillOval(50,110,50,50);
+		g.setColor(orange1);
+		g.fillOval(50,170,50,50);
+	}
+}
+public class TrafficDemo
+{
+	public static void main(String[] args)
+	{
+		JFrame f1=new JFrame();
+		f1.setVisible(true);
+		f1.setSize(500,500);
+		f1.setLayout(null);
+		TrafficLight t=new TrafficLight();
+		f1.add(t);
+	}
+}
+
